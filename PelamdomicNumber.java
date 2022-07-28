e6b402e1d1157c0be2d19da1990706235cefe5be
Rohit <Officialrohitsharma442@gmail.com>
2022-07-27 20:50:34 -0500

    made dynmic vaule in side of static row number

--- PelamdomicNumber.java
+++ PelamdomicNumber.java
@@ -1,8 +1,9 @@
 public class PelamdomicNumber {
     public static void main(String args[]) {
     //  pelandomic pattern
-     for(int i=1; i<=5; i++){
-        for(int j=1; j<=5-i; j++){
+    int n = 5;
+     for(int i=1; i<=n; i++){
+        for(int j=1; j<=n-i; j++){
             System.out.print(" ");
         }
         
