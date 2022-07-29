ac6c13e7d462c8d1cd7f32c5c11caaea38e7f74d
Rohit <Officialrohitsharma442@gmail.com>
2022-07-29 14:22:39 -0500

    correct of varriable name

--- PascalsTriangle.java
+++ PascalsTriangle.java
@@ -8,7 +8,7 @@
  */
 public class PascalsTriangle {
     public static void main(String args[]) {
-        int i=5;
+        int n=5;
         for(int i=1; i<=n; i++){
             for(int j=1; j<=n; j++){
                 System.out.println("*");
