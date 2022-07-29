c86e37aacb09cc7dc291d27394ec9f67016c69a7
Rohit <Officialrohitsharma442@gmail.com>
2022-07-29 14:21:53 -0500

    make basic performa of star parrern

--- PascalsTriangle.java
+++ PascalsTriangle.java
@@ -8,6 +8,11 @@
  */
 public class PascalsTriangle {
     public static void main(String args[]) {
-        System.out.println("holla");
+        int i=5;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                System.out.println("*");
+            }
+        }
     }
 }
