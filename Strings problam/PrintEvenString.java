945325cf12e28044885a2ba4e174e43d2199b213
Rohit <Officialrohitsharma442@gmail.com>
2022-08-25 15:29:08 +0530

    this progtam print all even length string

---
+++ Strings problam/PrintEvenString.java
@@ -0,0 +1,10 @@
+public class PrintEvenString {
+    public static void main(String[] args) {
+        String str = "hell world";
+        for(String s:str.split(" ")){
+            if(s.length()%2==0){
+                System.out.println(s);
+            }
+        }
+    }
+}
