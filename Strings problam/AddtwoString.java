d7c47f63e7067e9e61b7f7ee10a18438053f2e02
Rohit <Officialrohitsharma442@gmail.com>
2022-08-25 15:38:06 +0530

    and i print the integer value in console

--- Strings problam/AddtwoString.java
+++ Strings problam/AddtwoString.java
@@ -2,6 +2,7 @@ public class AddtwoString {
     public static void main(String[] args) {
          Sting str1 = "111";
          Sting str2 = "222";
-         int Str3 =  Integer.parseInt(str1)+Integer.parseInt(Str2);    
+         int Str3 =  Integer.parseInt(str1)+Integer.parseInt(Str2); 
+         System.out.println(Str3);   
     }
 }
