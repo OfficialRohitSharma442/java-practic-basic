bfc524da9264c0305711aad513605c6bc7a64583
Rohit <Officialrohitsharma442@gmail.com>
2022-08-25 16:01:08 +0530

    this program count how many word in string

---
+++ Strings problam/CountWordInString.java
@@ -0,0 +1,7 @@
+public class CountWordInString {
+    public static void main(String[] args) {
+        String str = "welcome to my github account";
+        String []arr = str.split(" ");
+        System.out.println(arr.length);
+    }
+}
