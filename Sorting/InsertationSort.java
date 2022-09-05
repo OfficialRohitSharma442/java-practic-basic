5b8dc4d6f252dabab39a289b9959830a3057cc67
Rohit <Officialrohitsharma442@gmail.com>
2022-09-05 20:20:09 +0530

    remove duplict element

--- Sorting/InsertationSort.java
+++ Sorting/InsertationSort.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class InsertationSort {
     public static void main(String[] args) {
-        int []arr ={9,6,3,6,4,5,8};
+        int []arr ={9,6,3,4,5,8};
         for(int i=0; i<arr.length; i++){
             int current = arr[i];
             int j =i-1;
@@ -11,8 +11,6 @@ public class InsertationSort {
             }
             /* placement  */
             arr[j+1]=current;
-           
-
         }
         System.out.println(Arrays.toString(arr));
      }
